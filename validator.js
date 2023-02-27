76a210a37c7babcc47dd912bb9ca0050799926d6
Whenpigsfly1 <60750501+Whenpigsfly1@users.noreply.github.com>
2023-02-26 23:21:40 -0500

    Update validator.js

--- validator.js
+++ validator.js
@@ -1,20 +1,17 @@
-const outputs = ['I\'m sorry', 'I understand', 'That\'s a tough situation', 'Tell me more', 'Go on', 'Why?'];
-
-const begOutputs = ['Tell me more', 'Go on', 'Why?', 'Please continue'];
+const outputs = ['I\'m sorry', 'I understand', 'That\'s a tough situation', 'Tell me more', 'Go on', 'Why?', 'Aww that\'s rough'];
 
 const endOutputs = ['Of course', 'Anytime', 'You\'re welcome'];
 
+const decisionOutputs = ['What do you think you should do?', 'What do you feel is the best path?', 'Only you can decide that, my friend'];
+
+const conundrumOutputs = ['I\'m not sure - what do you think?', 'I think that is quite a conundrum'];
+
 function pickAnOutput() {
-	numb = Math.floor(Math.random()*6);
+	numb = Math.floor(Math.random()*7);
 	response = outputs[numb];
 	return response;
 }
 
-function pickABegOutput() {
-  numb2 = Math.floor(Math.random()*4);
-  response = endOutputs[numb2];
-  return response;
-}
 
 function pickAnEndOutput() {
   numb3 = Math.floor(Math.random()*3);
@@ -22,11 +19,30 @@ function pickAnEndOutput() {
   return response;
 }
 
+function pickADecisionOutput() {
+  numb4 = Math.floor(Math.random()*3);
+  response = decisionOutputs[numb4];
+  return response;
+}
 
-function chooseOutput(input) {
+function pickAConundrumOutput() {
+  numb5 = Math.floor(Math.random()*5);
+  response = conundrumOutputs[numb4];
+  return response;
+}
+
+function chooseOutput(rawInput) {
+  let input = rawInput.toLowerCase();
   if (input == "hi" || input == "hello") {
     output = "Hello there. What would like to vent about?";}
-  else if (input == "Thank you" || input == "thank you" || input == "thanks") {
+  else if (input == "what do you think" || input == "what do you think?") {
+    output = pickAConundrumOutput();}
+  else if (input.includes("what should i do") == true) {
+    output = pickADecisionOutput();}
+  else if (input.includes("do you want to hear") == true || input.includes("do you want to listen") == true || input.includes("do you really want to listen") == true || input.includes("do you really want to hear") == true) {
+    output = "Yes, go on";
+  }
+  else if (input == "thank you" || input == "thanks" || input == "thanks for listening" || input == "thank you for listening" || input == "thank you so much") {
     output = pickAnEndOutput();}
   else {output = pickAnOutput();}
     return output;
